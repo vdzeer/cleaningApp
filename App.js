be85b13e3de0a7edc4c90da8f96ab9923ac04986
vdzeer <vdzerniuk@gmail.com>
2021-05-25 12:18:00 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <View />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
